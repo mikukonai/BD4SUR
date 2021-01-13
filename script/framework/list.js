93562093e23dac29130462b942e376bd85767dfb
憂鬱的烏龜 <20069428+mikukonai@users.noreply.github.com>
2021-01-14 00:42:59 +0800

    更新

--- script/framework/list.js
+++ script/framework/list.js
@@ -6,128 +6,6 @@
 // 载入并渲染列表
 function LoadList(pageId) {
 
-    // 标题→文件名
-    function TitleToFilename(title) {
-        return title.replace(/\s+/gi, "-");
-    }
-
-    // 渲染时间线
-    function RenderTimeline(articleList) {
-        let articles = new Object();
-        for(let i = 0; i < articleList.length; i++) {
-            let dates = new Array();
-            dates.push(articleList[i].date);
-            dates = dates.concat(articleList[i].updates);
-            for(let j = 0; j < dates.length; j++) {
-                let date = dates[j];
-                if(!articles[date]) {
-                    articles[date] = new Array();
-                }
-                articles[date].push(articleList[i]);
-            }
-        }
-
-        $("#WeekCountContainer").append(`<td class="WeekCount"></td>`);
-        $("#DaysContainer").append(`<td>
-            <div class="Weektag">月</div>
-            <div class="Weektag">火</div>
-            <div class="Weektag">水</div>
-            <div class="Weektag">木</div>
-            <div class="Weektag">金</div>
-            <div class="Weektag">土</div>
-            <div class="Weektag">日</div>
-        </td>`);
-
-        let date = new Date();
-        let currentMonth = date.getMonth() + 1;
-
-        let weekDayCount = 0;
-        let weekCount = 0;
-        let newYearDayWeek = new Date(date.getFullYear(), 0, 1).getDay(); // 当年元旦是星期几
-        newYearDayWeek = (newYearDayWeek === 0) ? 7 : newYearDayWeek;
-        const MONTH_DAYS = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        let isLeapYear = false;
-        let fullyear = date.getFullYear();
-        if(fullyear % 100 === 0) {
-            if(fullyear % 400 === 0) isLeapYear = true;
-            else isLeapYear = false;
-        }
-        else {
-            if(fullyear % 4 === 0) isLeapYear = true;
-            else isLeapYear = false;
-        }
-        let weekRowStr = `<td>`;
-
-        // 处理1月1日之前的空格（TODO 每年第一周的处理逻辑应对标ISO8601）
-        newYearDayWeek--;
-        while(newYearDayWeek > 0) {
-            weekRowStr += `<div class="Day Blank"></div>`;
-            weekDayCount++;
-            newYearDayWeek--;
-        }
-
-        for(let month = 1; month <= currentMonth; month++) { // 注意getMonth()返回的月份数字从0开始
-            monthDays = MONTH_DAYS[month];
-            if(month === 2 && isLeapYear === true) monthDays++;
-            for(let day = 1; day <= monthDays; day++) {
-                let firstDay = (day === 1) ? " Firstday" : "";
-                let monthString = ("00" + String(month)).slice(-2);
-                let dayString = ("00" + String(day)).slice(-2);
-                let dateString = `${date.getFullYear()}-${monthString}-${dayString}`;
-    
-                let level = "";
-                if(articles[dateString]) {
-                    let articleNumber = articles[dateString].length;
-                    if(articleNumber === 1) { level = " L1"; }
-                    else if(articleNumber === 2) { level = " L2"; }
-                    else if(articleNumber === 3) { level = " L3"; }
-                    else if(articleNumber  >= 4) { level = " L4"; }
-                }
-                else {
-                    level = "";
-                }
-                weekRowStr += `<div class="Day${firstDay}${level}" id="Article-${dateString}" data-date="${dateString}" title="${dateString}"></div>`;
-                weekDayCount++;
-                if(weekDayCount % 7 === 0) {
-                    weekCount++;
-                    weekRowStr += `</td>`;
-                    $("#DaysContainer").append(weekRowStr);
-                    $("#WeekCountContainer").append(`<td class="WeekCount">${weekCount}</td>`);
-                    weekRowStr = `<td>`;
-                }
-                else if(day === monthDays && month === currentMonth) {
-                    weekCount++;
-                    let blanks = 7 - weekDayCount % 7;
-                    if(blanks < 7) {
-                        for(let i = 0; i < blanks; i++) {
-                            weekRowStr += `<div class="Day Blank"></div>`;
-                        }
-                    }
-                    weekRowStr += `</td>`;
-                    $("#DaysContainer").append(weekRowStr);
-                    $("#WeekCountContainer").append(`<td class="WeekCount">${weekCount}</td>`);
-                    weekRowStr = `<td>`;
-                }
-            }
-        }
-
-        // 转到某个文章的位置
-        function TurnToArticle(title) {
-            let targetTop = window.pageYOffset + $(`div[data-title='${title}']`)[0].getBoundingClientRect().top;
-            $('html, body').animate({ scrollTop: targetTop-40 }, 200, 'easeOutExpo'); // 照顾顶部sticky导航栏的40px高度
-        }
-    
-        $(".Day").each((i, e) => {
-            $(e).click(() => {
-                let date = $(e).attr("data-date");
-                if(date in articles) {
-                    RenderArticleList(articles[date]);
-                    TurnToArticle(articles[date][0].title);
-                }
-            });
-        });
-    }
-
     // 列表渲染为HTML
     function RenderArticleList(articleList, listingMode) {
 
@@ -156,16 +34,15 @@ function LoadList(pageId) {
             let coverHtml = (!cover || cover.length <= 0) ? "" : `<img class="ArticleCover" src="${cover}">`;
 
             let html = `
-<div class="ArticleItem enter" data-title="${title}">
-    ${coverHtml}
-    <div class="ArticleTitle"><span class="ArticleTitleLink SPA_TRIGGER" data-target="${pageId}/${TitleToFilename(title)}">${title}</span></div>
-    <div class="ArticleInfo">${tagString}${category} / ${date}</div>
-</div>`;
+<tr class="ArticleItem enter" data-title="${title}">
+    <td class="ArticleTitle"><span class="ArticleTitleLink SPA_TRIGGER" data-target="${pageId}/${TitleToFilename(title)}">${title}</span></td>
+    <td class="ArticleInfo"><span class="ArticleTag">${category}</span></td>
+</tr>`;
 
             HtmlBuffer.push(html);
         }
 
-        document.getElementById('ListContainer').innerHTML = HtmlBuffer.join("");
+        document.getElementById('ArticleListContainer').innerHTML = HtmlBuffer.join("");
 
         // SlideInOneByOne("enter", 10, 1000, 5);
 
@@ -220,7 +97,7 @@ function LoadList(pageId) {
                 HtmlBuffer.push('</div>');
                 catCount++;
             }
-            document.getElementById('ListContainer').innerHTML = HtmlBuffer.join("");
+            document.getElementById('ArticleListContainer').innerHTML = HtmlBuffer.join("");
         }
 
         else {
@@ -241,7 +118,7 @@ function LoadList(pageId) {
                     }
                 }
             });
-            document.getElementById('ListContainer').innerHTML = `<div class="ListCategoryBlock">${RenderList(listObject.items)}</div>`;
+            document.getElementById('ArticleListContainer').innerHTML = `<div class="ListCategoryBlock">${RenderList(listObject.items)}</div>`;
         }
 
         // 淡入动画
@@ -263,7 +140,7 @@ function LoadList(pageId) {
             SPA_RegisterTriggers();
         }, 100); // 100ms节流
     });
-    LIST_OBSERVER.observe(document.getElementById('ListContainer'), {characterData: true, childList: true, subtree: true});
+    LIST_OBSERVER.observe(document.getElementById('ArticleListContainer'), {characterData: true, childList: true, subtree: true});
 
     let listEndingSlogan = $('.ListEnding').html();
     $('.ListEnding').html('正在读取，请稍等…');
--- script/framework/station_log.js
+++ script/framework/station_log.js
@@ -1,4 +1,128 @@
 function LoadStationLog() {
+
+
+    // 渲染时间线
+    function RenderTimeline(stationLog) {
+        let items = new Object();
+        for(let i = 0; i < stationLog.length; i++) {
+            let times = new Array();
+            times.push(stationLog[i].time.split(" ")[0]);
+            times = times.concat(stationLog[i].updates);
+            for(let j = 0; j < times.length; j++) {
+                let time = times[j];
+                if(!items[time]) {
+                    items[time] = new Array();
+                }
+                items[time].push(stationLog[i]);
+            }
+        }
+
+        $("#WeekCountContainer").append(`<td class="WeekCount"></td>`);
+        $("#DaysContainer").append(`<td>
+            <div class="Weektag">月</div>
+            <div class="Weektag">火</div>
+            <div class="Weektag">水</div>
+            <div class="Weektag">木</div>
+            <div class="Weektag">金</div>
+            <div class="Weektag">土</div>
+            <div class="Weektag">日</div>
+        </td>`);
+
+        let date = new Date();
+        let currentMonth = date.getMonth() + 1;
+
+        let weekDayCount = 0;
+        let weekCount = 0;
+        let newYearDayWeek = new Date(date.getFullYear(), 0, 1).getDay(); // 当年元旦是星期几
+        newYearDayWeek = (newYearDayWeek === 0) ? 7 : newYearDayWeek;
+        const MONTH_DAYS = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        let isLeapYear = false;
+        let fullyear = date.getFullYear();
+        if(fullyear % 100 === 0) {
+            if(fullyear % 400 === 0) isLeapYear = true;
+            else isLeapYear = false;
+        }
+        else {
+            if(fullyear % 4 === 0) isLeapYear = true;
+            else isLeapYear = false;
+        }
+        let weekRowStr = `<td>`;
+
+        // 处理1月1日之前的空格（TODO 每年第一周的处理逻辑应对标ISO8601）
+        newYearDayWeek--;
+        while(newYearDayWeek > 0) {
+            weekRowStr += `<div class="Day Blank"></div>`;
+            weekDayCount++;
+            newYearDayWeek--;
+        }
+
+        for(let month = 1; month <= currentMonth; month++) { // 注意getMonth()返回的月份数字从0开始
+            monthDays = MONTH_DAYS[month];
+            if(month === 2 && isLeapYear === true) monthDays++;
+            for(let day = 1; day <= monthDays; day++) {
+                let firstDay = (day === 1) ? " Firstday" : "";
+                let monthString = ("00" + String(month)).slice(-2);
+                let dayString = ("00" + String(day)).slice(-2);
+                let dateString = `${date.getFullYear()}-${monthString}-${dayString}`;
+
+                let level = "";
+                if(items[dateString]) {
+                    let articleNumber = items[dateString].length;
+                    if(articleNumber === 1) { level = " L1"; }
+                    else if(articleNumber === 2) { level = " L2"; }
+                    else if(articleNumber === 3) { level = " L3"; }
+                    else if(articleNumber  >= 4) { level = " L4"; }
+                }
+                else {
+                    level = "";
+                }
+                weekRowStr += `<div class="Day${firstDay}${level}" id="Article-${dateString}" data-time="${dateString}" title="${dateString}"></div>`;
+                weekDayCount++;
+                if(weekDayCount % 7 === 0) {
+                    weekCount++;
+                    weekRowStr += `</td>`;
+                    $("#DaysContainer").append(weekRowStr);
+                    $("#WeekCountContainer").append(`<td class="WeekCount">${weekCount}</td>`);
+                    weekRowStr = `<td>`;
+                }
+                else if(day === monthDays && month === currentMonth) {
+                    weekCount++;
+                    let blanks = 7 - weekDayCount % 7;
+                    if(blanks < 7) {
+                        for(let i = 0; i < blanks; i++) {
+                            weekRowStr += `<div class="Day Blank"></div>`;
+                        }
+                    }
+                    weekRowStr += `</td>`;
+                    $("#DaysContainer").append(weekRowStr);
+                    $("#WeekCountContainer").append(`<td class="WeekCount">${weekCount}</td>`);
+                    weekRowStr = `<td>`;
+                }
+            }
+        }
+
+        // 转到某个文章的位置
+        function TurnToItem(date) {
+            let targetTop = window.pageYOffset + $(`tr[data-date='${date}']`)[0].getBoundingClientRect().top;
+            $('html, body').animate({ scrollTop: targetTop-40 }, 200, 'easeOutExpo'); // 照顾顶部sticky导航栏的40px高度
+        }
+
+        $(".Day").each((i, e) => {
+            $(e).click(() => {
+                let time = $(e).attr("data-time");
+                if(time in items) {
+                    TurnToItem(time);
+                }
+            });
+        });
+    }
+
+
+
+
+
+
+
     let xhr = new XMLHttpRequest();
     xhr.open("GET", `./station_log.json`);
     xhr.onreadystatechange = () => {
@@ -6,36 +130,40 @@ function LoadStationLog() {
             let log = JSON.parse(xhr.responseText);
             for(let i = 0; i < log.length; i++) {
                 let item = log[i];
+                let date = item.time.split(" ")[0];
+                let time = item.time.split(" ")[1];
                 $("#StationLogTable").append(`
-    <tr>
-        <td>${item.time}</td>
+    <tr data-date="${date}">
+        <td>${date}<br>${time}</td>
         <td>${item.callsign}</td>
         <td>
             <table class="InfoTable">
-                <tr><th class="LogColoumInfo">FREQ</th><td class="LogColoumInfoValue">${item.freq}</td></tr>
-                <tr><th class="LogColoumInfo">MODE</th><td class="LogColoumInfoValue">${item.mode}</td></tr>
-                <tr><th class="LogColoumInfo">RST</th><td class="LogColoumInfoValue">${item.rst}</td></tr>
+                <tr><td class="LogColoumInfo">FREQ</td><td class="LogColoumInfoValue">${item.freq}</td></tr>
+                <tr><td class="LogColoumInfo">MODE</td><td class="LogColoumInfoValue">${item.mode}</td></tr>
+                <tr><td class="LogColoumInfo">RST</td><td class="LogColoumInfoValue">${item.rst}</td></tr>
             </table>
         </td>
         <td>
             <table class="InfoTable">
-                <tr><th class="LogColoumInfo">RIG</th><td class="LogColoumInfoValue">${item.rig}</td></tr>
-                <tr><th class="LogColoumInfo">ANT</th><td class="LogColoumInfoValue">${item.ant}</td></tr>
-                <tr><th class="LogColoumInfo">PWR</th><td class="LogColoumInfoValue">${item.pwr}</td></tr>
-                <tr><th class="LogColoumInfo">QTH</th><td class="LogColoumInfoValue">${item.qth}</td></tr>
+                <tr><td class="LogColoumInfo">RIG</td><td class="LogColoumInfoValue">${item.rig}</td></tr>
+                <tr><td class="LogColoumInfo">ANT</td><td class="LogColoumInfoValue">${item.ant}</td></tr>
+                <tr><td class="LogColoumInfo">PWR</td><td class="LogColoumInfoValue">${item.pwr}</td></tr>
+                <tr><td class="LogColoumInfo">QTH</td><td class="LogColoumInfoValue">${item.qth}</td></tr>
             </table>
         <td>
             <table class="InfoTable">
-                <tr><th class="LogColoumInfo">RIG</th><td class="LogColoumInfoValue">${item.rig_self}</td></tr>
-                <tr><th class="LogColoumInfo">ANT</th><td class="LogColoumInfoValue">${item.ant_self}</td></tr>
-                <tr><th class="LogColoumInfo">PWR</th><td class="LogColoumInfoValue">${item.pwr_self}</td></tr>
-                <tr><th class="LogColoumInfo">QTH</th><td class="LogColoumInfoValue">${item.qth_self}</td></tr>
+                <tr><td class="LogColoumInfo">RIG</td><td class="LogColoumInfoValue">${item.rig_self}</td></tr>
+                <tr><td class="LogColoumInfo">ANT</td><td class="LogColoumInfoValue">${item.ant_self}</td></tr>
+                <tr><td class="LogColoumInfo">PWR</td><td class="LogColoumInfoValue">${item.pwr_self}</td></tr>
+                <tr><td class="LogColoumInfo">QTH</td><td class="LogColoumInfoValue">${item.qth_self}</td></tr>
             </table>
         </td>
         <td class="LogColoumNote">${item.note}</td>
     </tr>
                 `);
             }
+
+            RenderTimeline(log);
         }
         else if(xhr.readyState === XMLHttpRequest.DONE && xhr.status !== 200){
             $("#StationLogTable").html("没有找到日志数据...");
